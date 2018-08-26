245001717b2625c8eb8f4c26054a872a5572ba33
Ahmed <AhmedElsawyrev@gmail.com>
2018-08-26 05:14:42 +0200

    feat: a new test suite named "Initial Entries"

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -84,6 +84,11 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function () {
+        beforeEach(function(done) {
+            loadFeed(0);
+            done();
+        });
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
