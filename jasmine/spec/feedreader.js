c8083710ab2598e4ad2fb3ae4522119017c69dc6
Ahmed <AhmedElsawyrev@gmail.com>
2018-08-26 05:13:02 +0200

    feat: a test that ensures the menu changes visibility when the menu icon is clicked

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -73,6 +73,15 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('changes visibility when clicked' , function () {
+        if (callBack.calls.count() % 2 === 0) {
+            expect(body.hasClass('menu-hidden')).toBe(true);
+        }else {
+            expect(body.hasClass('menu-hidden')).toBe(false);
+        }
+        });
+
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
